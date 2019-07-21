498660b06bf977a5a52a324dced7b79a6d283ea5
unknown <mail.mfernandez@gmail.com>
2019-07-20 23:03:28 -0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
-const path = require('path');
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// Serve static files
-app.use(express.static(__dirname + '/dist/spotiapp'));
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/spotiapp'));
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/spotiapp/index.html'));
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/spotiapp/index.html'));
 });
 
-// default Heroku port
-app.listen(process.env.PORT || 5000);
-// Heroku automagically gives us SSL
-// Lets write some middleware to redirect us
-let env = process.env.NODE_ENV || 'development';
-
-let forceSSL = (req, res, next) => {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
-    return res.redirect(['https://', req.get('Host'), req.url].join(''));
-  }
-  return next();
-};
-
-if (env === 'production') {
-  app.use(forceSSL);
-}
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
