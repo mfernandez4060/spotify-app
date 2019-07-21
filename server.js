fcc829c8d8ac0a0e6f4a480e7b66f646af6ff2a2
unknown <mail.mfernandez@gmail.com>
2019-07-21 20:45:05 -0300

    error in package.json fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-var allowCrossDomain = function(req, res, next) {
+var allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
@@ -18,23 +18,21 @@ var allowCrossDomain = function(req, res, next) {
   }
 };
 
-app.configure(function () {
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(allowCrossDomain);
-  app.use(express.static(path.join(application_root, "public")));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(allowCrossDomain);
+app.use(express.static(path.join(application_root, "public")));
+app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 
 
 
 // Serve only the static files form the spotiapp directory
 app.use(express.static(__dirname + '/spotiapp'));
 
-app.get('/*', function(req,res) {
+app.get('/*', function (req, res) {
 
-res.sendFile(path.join(__dirname+'/spotiapp/index.html'));
+  res.sendFile(path.join(__dirname + '/spotiapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
