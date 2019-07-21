165daac4bbbfc0a52c7cbf4e09fd007b227090c2
unknown <mail.mfernandez@gmail.com>
2019-07-21 20:39:11 -0300

    error in package.json fix

--- server.js
+++ server.js
@@ -4,6 +4,31 @@ const path = require('path');
 
 const app = express();
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(allowCrossDomain);
+  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+
+
 // Serve only the static files form the spotiapp directory
 app.use(express.static(__dirname + '/spotiapp'));
 
